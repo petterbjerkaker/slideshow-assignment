b7bbf2bd5b542c8d8c6e4ae47a4a0f590df3564e
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2023-10-10 17:26:09 +0200

    Add js events click icons

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const slideshow = document.querySelector(".slideshow");
+const slideshow = document.querySelector(".slideshow"),
+firstImg = slideshow.querySelectorAll("img")[0];
+angleIcons = document.querySelectorAll(".container i");
 
-const dragging = ()=>	{
-	
+let firstImgWidth = firstImg.clientWidth + 10;
+
+angleIcons.forEach(icon => {
+	icon.addEventListener("click", ()=>{
+		if(icon.id == "left") {
+			slideshow.scrollLeft -= firstImgWidth;
+		}else {
+			slideshow.scrollLeft += firstImgWidth;
+		}
+	});
+});
+
+const dragging = (e)=>	{
+	slideshow.scrollLeft = e.pageX;
 }
 
 slideshow.addEventListener("mousemove", dragging);
