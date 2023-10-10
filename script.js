adccd3b0f0634509d2c67389bcd21cb69669bd2d
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2023-10-10 17:44:10 +0200

    Add drag event when clicking down mousebtn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const slideshow = document.querySelector(".slideshow"),
 firstImg = slideshow.querySelectorAll("img")[0];
 angleIcons = document.querySelectorAll(".container i");
 
+
 let firstImgWidth = firstImg.clientWidth + 10;
 
 angleIcons.forEach(icon => {
@@ -14,8 +15,25 @@ angleIcons.forEach(icon => {
 	});
 });
 
-const dragging = (e)=>	{
-	slideshow.scrollLeft = e.pageX;
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e)=> {
+	isDragStart = true;
+	prevPageX = e.pageX;
+	prevScrollLeft = slideshow.scrollLeft;
 }
 
+const dragging = (e)=>	{
+	if(!isDragStart) return;
+	e.preventDefault();
+	let positionDiff = e.pageX - prevPageX;
+	slideshow.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = ()=>{
+	isDragStart = false;
+}
+
+slideshow.addEventListener("mousedown", dragStart);
 slideshow.addEventListener("mousemove", dragging);
+slideshow.addEventListener("mouseup", dragStop);
